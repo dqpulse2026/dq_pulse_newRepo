ecc8d1d2c1ecb8678c1c3fcea27f5f9e41f34b97
GA Atf 1 <gaatf.1@lsdigital.com>
2026-06-02 12:36:10 +0530

    Configure project for web_v2 GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/DQ_Pulse_web/',
+  base: '/web_v2/',
   plugins: [react()],
 });
