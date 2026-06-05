23c1674c05b133c4731c3cfb7bf4fd85165ef6c9
GA Atf 2 <gaatf.2@lsdigital.com>
2026-06-05 12:26:21 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/web_v2/',
+  base: '/dq_pulse_newRepo/',
   plugins: [react()],
 });
